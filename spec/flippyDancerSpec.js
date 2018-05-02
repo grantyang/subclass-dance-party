aaaef3a434bd7cafc53e379a32042abf323a2b4e
grantyang <grantyang1@gmail.com>
2018-05-01 19:11:16 -0700

    wrote tests

--- spec/flippyDancerSpec.js
+++ spec/flippyDancerSpec.js
@@ -1,21 +1,22 @@
-describe('flippyDancer', function() {
+describe('flippyDancer', function () {
 
   var flippyDancer, clock;
   var timeBetweenSteps = 100;
 
-  beforeEach(function() {
+  beforeEach(function () {
     clock = sinon.useFakeTimers();
     flippyDancer = new makeFlippyDancer(10, 20, timeBetweenSteps);
   });
 
-  it('should have a jQuery $node object', function() {
+  it('should have a jQuery $node object', function () {
     expect(flippyDancer.$node).to.be.an.instanceof(jQuery);
   });
 
-  it('should have a step function that makes its node blink', function() {
-    sinon.spy(flippyDancer.$node, 'toggle');
+  it('should move back and forth', function () {
     flippyDancer.step();
-    expect(flippyDancer.$node.toggle.called).to.be.true;
+    var location2 = setTimeout(function () {
+      return flippyDancer.$node.css('left');
+    }, 5)
+    expect(flippyDancer.$node.css('left') === location2).to.equal.false;
   });
-
 });
--- spec/growyDancerSpec.js
+++ spec/growyDancerSpec.js
@@ -1,33 +1,16 @@
-describe('blinkyDancer', function() {
+describe('growyDancer', function() {
 
-  var blinkyDancer, clock;
+  var growyDancer, clock;
   var timeBetweenSteps = 100;
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = new makeBlinkyDancer(10, 20, timeBetweenSteps);
+    growyDancer = new makeGrowyDancer(10, 20, timeBetweenSteps);
   });
 
   it('should have a jQuery $node object', function() {
-    expect(blinkyDancer.$node).to.be.an.instanceof(jQuery);
+    expect(growyDancer.$node).to.be.an.instanceof(jQuery);
   });
 
-  it('should have a step function that makes its node blink', function() {
-    sinon.spy(blinkyDancer.$node, 'toggle');
-    blinkyDancer.step();
-    expect(blinkyDancer.$node.toggle.called).to.be.true;
-  });
 
-  describe('dance', function() {
-    it('should call step at least once per second', function() {
-      sinon.spy(blinkyDancer, 'step');
-      expect(blinkyDancer.step.callCount).to.be.equal(0);
-      clock.tick(timeBetweenSteps);
-
-      expect(blinkyDancer.step.callCount).to.be.equal(1);
-
-      clock.tick(timeBetweenSteps);
-      expect(blinkyDancer.step.callCount).to.be.equal(2);
-    });
-  });
 });
